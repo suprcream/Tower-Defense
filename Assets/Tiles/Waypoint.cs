e8b59101ba296cbe3bf3d8bb04bcfe12e2d57eae
suprcream <suprcream.gg@gmail.com>
2022-12-01 00:25:31 +0700

    a

---
+++ Assets/Tiles/Waypoint.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoint : MonoBehaviour
+{
+    [SerializeField] bool isPlacable;
+
+    private void OnMouseDown() {
+        if (isPlacable) {
+            Debug.Log(transform.name);
+        }
+    }
+
+}
